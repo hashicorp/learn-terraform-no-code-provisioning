3f45ed70ce8ecc6774fa8b6b92f0379e7cbd1d3b
Brian McClain <brianmmcclain@gmail.com>
2024-04-22 15:30:06 -0400

    Bump engine version

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_db_instance" "education" {
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
-  engine_version         = "15.3"
+  engine_version         = "15.6"
   username               = var.db_username
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.education.name
